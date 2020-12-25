07fd9008a223057e8f5155b70a7d0fba4cea5a7a
Mitsuya Watanabe <mitsuya.watanabe85@gmail.com>
2020-12-25 22:48:14 +0100

    Add background.

--- App.js
+++ App.js
@@ -7,9 +7,125 @@
  */
 
 import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  ImageBackground,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
-  return <></>;
+  return (
+    <>
+      <Image
+        source={require('./assets/images/bg.png')}
+        style={{
+          width: '100%',
+          height: '100%',
+          top: 0,
+          zIndex: 0,
+          position: 'absolute',
+          backgroundColor: 'yellow',
+          resizeMode: 'contain',
+        }}
+      />
+      <View style={styles.scrollView}>
+        <Image
+          source={require('./assets/images/tinko.png')}
+          style={{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 2,
+            resizeMode: 'contain',
+          }}
+        />
+      </View>
+
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    position: 'relative',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  background: {
+    backgroundColor: '#FFCC24',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+
+    top: '50%',
+    zIndex: 1,
+  },
+  bg: {
+    overflow: 'visible',
+    resizeMode: 'contain',
+    /*
+     * These negative margins allow the image to be offset similarly across screen sizes and component sizes.
+     *
+     * The source logo.png image is 512x512px, so as such, these margins attempt to be relative to the
+     * source image's size.
+     */
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: Colors.black,
+  },
+});
 
 export default App;
